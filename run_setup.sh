7e4206bbc65a40768bf1c4fd3d412e8e85973878
lab-ubuntu <pengfeigaothu@gmail.com>
2017-02-28 17:18:11 +0800

    Draft

---
+++ run_setup.sh
@@ -0,0 +1,4 @@
+python setup.py sdist
+tar -xf dist/qrstype-0.1.tar.gz -C dist
+sudo python dist/qrstype-0.1/setup.py install
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='qrstype',
+      version='0.1',
+      description='ECG QRS type detector',
+      author='Phil G',
+      email = 'areseye@163.com',
+      packages=['qrstype'],
+     )
